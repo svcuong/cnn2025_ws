33df6265cfeac9c0411c9a968a95966a23193834
svcuong <svcuonghvktqs@gmail.com>
2026-01-21 14:21:45 +0700

    commit 21/01/2026

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,15 @@
+# CMake generated Testfile for 
+# Source directory: /home/cuong/cnn2025_ws/src
+# Build directory: /home/cuong/cnn2025_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("bringup")
+subdirs("interface")
+subdirs("monitoring")
+subdirs("navigation")
+subdirs("sync")
+subdirs("ai_perception")
+subdirs("localization")
+subdirs("sensors")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/cuong/cnn2025_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/cuong/cnn2025_ws/devel;/home/cuong/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/cuong/cnn2025_ws/devel;/home/cuong/catkin_ws/devel;/opt/ros/noetic' != '' else []
