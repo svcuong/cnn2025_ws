0f3e00dd81d23ef8ef2df422bdfee7e442e23d92
bonx771 <bonx771@gmail.com>
2026-01-22 16:29:17 +0700

    push lib camera

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,7 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/xuanbo/cnn2025_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu'
+export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig:/opt/ros/noetic/lib/x86_64-linux-gnu/pkgconfig'
 export PWD='/home/xuanbo/cnn2025_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/xuanbo/cnn2025_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/xuanbo/cnn2025_ws/src:$ROS_PACKAGE_PATH"
+export PYTHONPATH='/opt/ros/noetic/lib/python3/dist-packages'
